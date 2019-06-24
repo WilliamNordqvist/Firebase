37c90270358c18239cd3c7e949b579030713aded
willmister <wnordqvist@gmail.com>
2019-06-24 21:45:00 +0200

    check for dup in firebase

--- birthday.js
+++ birthday.js
@@ -20,38 +20,53 @@ onLoad();
 
 const form = (event) => {
     event.preventDefault();
+ 
     
     arr = [{
-        name:document.querySelector('#name').value,
-        guest:document.querySelector('#guest').value,
-        email:document.querySelector('#email').value,
-        text:document.querySelector('textarea').value
+        name:document.querySelector('#name').value.toLowerCase().trim(),
+        guest:document.querySelector('#guest').value.toLowerCase().trim(),
+        email:document.querySelector('#email').value.toLowerCase().trim(),
+        text:document.querySelector('textarea').value.toLowerCase().trim()
     }]
+
+    if(arr[0].guest === ''){
+
+    }
+    let name = arr[0].name;
+    let guest = arr[0].guest;
      
-    
- 
-    let name = document.querySelector('#name').value
-    let guest = document.querySelector('#guest').value   
+  
        console.log(arr)   
        
 
-    checkForDup();
+    checkForDup(name, guest);
     
    
      
 } 
 
 
-const checkForDup = () => {
-    let nameList = data.map(i => i.name.toLocaleUpperCase())
+const checkForDup = (name, guest) => {
+    let nameList = data.map(i => i.name)
 
-    if(nameList.includes(arr[0].name.toLocaleUpperCase())){
+    if(nameList.includes(name)){
         console.log('hitta match')
+        let matchingObj = data.filter(i => i.name === name)
+        console.log(matchingObj)
+
+        name = matchingObj[0].name
+        guest = matchingObj[0].guest
+
+        document.querySelector('.registeredUser').children[0].innerHTML = 'Du har redan anmält'
+        document.querySelector('.deleteUser').classList.remove("hidden");
+
+        
     } else {
         firebaseRef.child(arr[0].name).set(arr[0])
 
-        document.querySelector('.registeredUser').classList.remove("hidden");
-        document.querySelector('#nameOne').innerHTML = '1.' + arr[0].name
-        document.querySelector('#nameTwo').innerHTML = '2.' + arr[0].guest
     }
+    console.log(guest)
+    document.querySelector('.registeredUser').classList.remove("hidden");
+    document.querySelector('#nameOne').innerHTML = '1.' + name
+    document.querySelector('#nameTwo').innerHTML = '2.' + guest
 }
