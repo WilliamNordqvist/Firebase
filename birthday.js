bb0eb2eb3e447294188513fa71dd179b5cc9995b
willmister <wnordqvist@gmail.com>
2019-06-24 22:49:58 +0200

    small fixes

--- birthday.js
+++ birthday.js
@@ -1,16 +1,19 @@
 console.log('SIDAN STARTAD')
 
 let data = null;
+let test = null;
 let arr = []
 var firebaseRef = firebase.database().ref();
+let name = '';
+let guest = '';
 
 const onLoad = () => {
     
     firebaseRef.on("value", function(snapshot) {
          data = snapshot.val()
          data = Object.values(data)
-       console.log(data);
     });
+
 
 }
 
@@ -32,8 +35,8 @@ const form = (event) => {
     if(arr[0].guest === ''){
 
     }
-    let name = arr[0].name;
-    let guest = arr[0].guest;
+    name = arr[0].name;
+     guest = arr[0].guest;
      
   
        console.log(arr)   
@@ -47,26 +50,51 @@ const form = (event) => {
 
 
 const checkForDup = (name, guest) => {
-    let nameList = data.map(i => i.name)
 
-    if(nameList.includes(name)){
-        console.log('hitta match')
-        let matchingObj = data.filter(i => i.name === name)
-        console.log(matchingObj)
 
-        name = matchingObj[0].name
-        guest = matchingObj[0].guest
+    if(data !== null){
+        let nameList = data.map(i => i.name)
 
-        document.querySelector('.registeredUser').children[0].innerHTML = 'Du har redan anmält'
-        document.querySelector('.deleteUser').classList.remove("hidden");
+        if(nameList.includes(name)){
+            console.log('hitta match')
+            let matchingObj = data.filter(i => i.name === name)
+            console.log(matchingObj)
+    
+            name = matchingObj[0].name
+            guest = matchingObj[0].guest
+    
+            document.querySelector('.registeredUser').children[0].innerHTML = 'Du har redan anmält'
+            document.querySelector('.registeredUser').classList.remove("hidden");
+            document.querySelector('.deleteUser').classList.remove("hidden");
+            } else {
 
-        
+            document.querySelector('.registeredUser').classList.remove("hidden");
+            document.querySelector('#nameOne').innerHTML = '1.' + name
+            document.querySelector('#nameTwo').innerHTML = '2.' + guest
+            }
     } else {
-        firebaseRef.child(arr[0].name).set(arr[0])
 
+        console.log('guest')
+        firebaseRef.child(arr[0].name).set(arr[0])
+        document.querySelector('.registeredUser').classList.remove("hidden");
+        document.querySelector('#nameOne').innerHTML = '1.' + name
+        document.querySelector('#nameTwo').innerHTML = '2.' + guest
     }
-    console.log(guest)
-    document.querySelector('.registeredUser').classList.remove("hidden");
-    document.querySelector('#nameOne').innerHTML = '1.' + name
-    document.querySelector('#nameTwo').innerHTML = '2.' + guest
+
+    } 
+   
+
+
+
+const deletUser = (name) => {
+    firebase.database().ref().child(arr[0].name).remove()
+  .then(function() {
+    alert("bokning raderad")
+  })
+  .catch(function(error) {
+    console.log("Remove failed: " + error.message)
+  });
+
+  document.querySelector('.registeredUser').classList.add("hidden");
+
 }
